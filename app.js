7251edf597f4e6edfe2dfaa16ffe901f497a0b2f
NathanXeroso <natanaellean2@gmail.com>
2025-01-06 18:56:39 -0300

    Mudança no lugar onde é chamada a função

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
+exibirMensagemInicial();
+
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -13,8 +15,6 @@ function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e ' . numeroLimite);
 }
-
-exibirMensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
