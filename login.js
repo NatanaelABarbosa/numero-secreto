6452dcf3f952227ce8b75065a043fb9f98eee641
Natanael Leandro Alves Barbosa <1658938@sga.pucminas.br>
2026-04-17 15:48:23 -0300

    projeto_final

--- login.js
+++ login.js
@@ -1,1 +1,15 @@
-let username = document.getElementsByClassName()
+const form = document.querySelector("form");
+const erro = document.getElementById("erro");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username == "natanael" && password == "1234") {
+        window.location.href="home.html";
+    } else {
+        erro.textContent = "Usuário ou senha inválidos (coloque o texto transparente em cada parte)";
+        erro.style.display = "block";
+    }
+});
